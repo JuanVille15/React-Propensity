bbd318c79bc8a78fedbf0586b1edeb5f17a4cb70
JuanVille15 <juanville9915@gmail.com>
2026-09-13 11:10:02 -0500

    Se incorpora estado Receso: 17

--- sql/labels.sql
+++ sql/labels.sql
@@ -11,7 +11,7 @@ WITH INACTIVOS_MES_ANTERIOR AS (
 		AND dime.strFuente ='CooTaylor'
 		AND dime.strDescEstadoFuente ='Inactivo'
 	WHERE
-		BodegaCorporativa.$partition.Pf_mes(fct.dtmfechainsercion) =  BodegaCorporativa.$partition.pf_mes(?)
+		BodegaCorporativa.$partition.Pf_mes(fct.dtmfechainsercion) =  BodegaCorporativa.$partition.pf_mes('2026-07-01')
 		AND fct.[indregistroactual] = 1 
 		AND fct.strfuente = 'CooTaylor'
 )
@@ -21,7 +21,7 @@ SELECT
 	ma.strcodestadoclientefuente AS [strEstadoInicial], 
 	CASE 
 		WHEN ma.strDescEstadoFuente = 'Inactivo' 
-		AND fct.strcodestadoclientefuente IN (10,11) THEN 1
+		AND fct.strcodestadoclientefuente IN (10,11,17) THEN 1
 		ELSE
 			0
 	END AS 'IndicadorReactivado'
@@ -35,7 +35,7 @@ ON
 	AND dime.numCodTipoEstado = 1
 	AND dime.strFuente ='CooTaylor'
 WHERE
-	BodegaCorporativa.$partition.Pf_mes(fct.dtmfechainsercion) =  BodegaCorporativa.$partition.pf_mes(?)
+	BodegaCorporativa.$partition.Pf_mes(fct.dtmfechainsercion) =  BodegaCorporativa.$partition.pf_mes('2026-08-01')
 	AND fct.[indregistroactual] = 1 
 	AND fct.strfuente = 'CooTaylor'
 	AND ma.strcodestadoclientefuente = 14;
