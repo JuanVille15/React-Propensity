6b16326bb5d939bb676b87d6244936c4831892d9
JuanVille15 <juanville9915@gmail.com>
2026-09-21 12:05:53 -0500

    Consulta Demografica

---
+++ sql/demografica.sql
@@ -0,0 +1,37 @@
+WITH FacSegmentos AS (
+                         SELECT
+                            strPeriodo AS Periodo,
+                            strIdentificacion AS ID,
+                            numEdad as Edad,
+                            numCantidadAniosAntiguedad as Antiguedad,
+			                numCantidadProductos as suma_productos
+                         FROM BodegaCorporativa.bodega.factSegmentos
+                         WHERE BodegaCorporativa.$partition.pf_mes(dtmFechaInsercion) = BodegaCorporativa.$partition.pf_mes(?)
+                    )
+                    SELECT
+                        dem.strPeriodo as Periodo,
+                        dem.Documento as ID,
+						dem.Tipo_Documento,
+		                dem.Nombre_Tipo_Vinculacion as Tipo_Vinculacion,
+		                dem.Estado_Civil,
+		                dem.Personas_a_Cargo,
+		                dem.Personas_a_Cargo_Menores_18,
+		                dem.Sexo,
+		                dem.Estrato,
+		                dem.Nombre_Tipo_Vivienda,
+		                dem.Nombre_Nivel_Academico,
+		                dem.Ingresos,
+		                dem.Egresos,
+		                dem.Nombre_Ocupacion,
+		                dem.Ptaje_acierta,
+		                dem.Cuotas_canceladas_aportes,
+						dem.Saldo_aportes as Saldoaportes,
+		                dem.Segmento_Ciclo_de_Vida,
+		                seg.Edad,
+		                seg.Antiguedad,
+		                seg.suma_productos
+                    FROM BodegaCorporativa.Conocimiento.v_Demografica Dem
+                    INNER JOIN FacSegmentos seg
+                        ON dem.strPeriodo = seg.Periodo 
+                        AND dem.Documento = seg.ID
+                    WHERE BodegaCorporativa.$partition.pf_mes(dem.dtmFechaInsercion) = BodegaCorporativa.$partition.pf_mes(?)
